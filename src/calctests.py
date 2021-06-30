9dab965228339e553b83a8a364942019ea400edd
Preyasha <preyashapatel2328@gmail.com>
2021-06-30 06:41:50 -0400

    Task: setUp and tearDown added

--- src/calctests.py
+++ src/calctests.py
@@ -1,25 +1,28 @@
 import unittest
-
 from calc import Calc
 
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calc = Calc()
+
     def test_instantiate_calc(self):
-        calc = Calc()
-        self.assertIsInstance(calc, Calc)
+        self.assertIsInstance(self.calc, Calc)
 
     def test_results_property_calc(self):
-        calc = Calc()
-        self.assertEqual(calc.result, 0)
+        self.assertEqual(self.calc.result, 0)
 
     def test_addition_method_calc(self):
-        calc = Calc()
-        self.assertEqual(calc.add(2,3), 5)
+        self.assertEqual(self.calc.add(2,3), 5)
+        self.assertEqual(self.calc.result, 5)
 
     def test_subtraction_method_calc(self):
-        calc = Calc()
-        self.assertEqual(calc.subtract(7,4), 3)
+        self.assertEqual(self.calc.subtract(7,4), 3)
+        self.assertEqual(self.calc.result, 3)
+
+    def tearDown(self) -> None:
+        pass
 
 if __name__ == "__main__":
     unittest.main()
