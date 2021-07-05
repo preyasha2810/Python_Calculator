f7019b07e917c87f8e6de31f12f6f2a078b622cd
Preyasha <preyashapatel2328@gmail.com>
2021-07-05 14:21:47 -0400

    Task: calctests updated

--- src/calctests.py
+++ src/calctests.py
@@ -29,13 +29,13 @@ class MyTestCase(unittest.TestCase):
     def test_division_method_calc(self):
         test_files = CsvReader("/src/files/Unit Test Div.csv").get_data
         for row in test_files:
-            self.assertAlmostEqual(self.calc.div(row['Value 2'], row['Value 1']), float(row['Result']))
+            self.assertAlmostEqual(self.calc.divide(row['Value 2'], row['Value 1']), float(row['Result']))
             self.assertAlmostEqual(self.calc.result, float(row['Result']))
 
     def test_multiplication_method_calc(self):
         test_files = CsvReader("/src/files/Unit Test Mult.csv").get_data
         for row in test_files:
-            self.assertEqual(self.calc.mult(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calc.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calc.result, int(row['Result']))
 
     def test_square_method_calc(self):
@@ -47,7 +47,7 @@ class MyTestCase(unittest.TestCase):
     def test_squareroot_method_calc(self):
         test_files = CsvReader("/src/files/Unit Test Square root.csv").get_data
         for row in test_files:
-            self.assertAlmostEqual(self.calc.sqr_root(row['Value 1']), float(row['Result']))
+            self.assertAlmostEqual(self.calc.squareroot(row['Value 1']), float(row['Result']))
             self.assertAlmostEqual(self.calc.result, float(row['Result']))
 
     def tearDown(self) -> None:
