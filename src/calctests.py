cbc1ea2fa50691e5b53925edf7a99caf6adf2280
Preyasha <preyashapatel2328@gmail.com>
2021-07-05 18:11:12 -0400

    Task: Staticproperties file added

--- src/calctests.py
+++ src/calctests.py
@@ -1,7 +1,7 @@
 import unittest
 from calc import Calc
 from csvReader import CsvReader
-
+from StaticVariable import StaticVariable
 
 class MyTestCase(unittest.TestCase):
 
@@ -15,40 +15,40 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calc.result, 0)
 
     def test_addition_method_calc(self):
-        test_files = CsvReader("/src/files/Unit Test Add.csv").get_data
+        test_files = CsvReader(StaticVariable.unit_Test_Add).get_data
         for row in test_files:
-            self.assertEqual(self.calc.add(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calc.result, int(row['Result']))
+            self.assertEqual(self.calc.add(row[StaticVariable.value_1], row[StaticVariable.value_2]), int(row[StaticVariable.result]))
+            self.assertEqual(self.calc.result, int(row[StaticVariable.result]))
 
     def test_subtraction_method_calc(self):
-        test_files = CsvReader("/src/files/Unit Test Sub.csv").get_data
+        test_files = CsvReader(StaticVariable.unit_Test_Sub).get_data
         for row in test_files:
-            self.assertEqual(self.calc.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calc.result, int(row['Result']))
+            self.assertEqual(self.calc.subtract(row[StaticVariable.value_1], row[StaticVariable.value_2]), int(row[StaticVariable.result]))
+            self.assertEqual(self.calc.result, int(row[StaticVariable.result]))
 
     def test_division_method_calc(self):
-        test_files = CsvReader("/src/files/Unit Test Div.csv").get_data
+        test_files = CsvReader(StaticVariable.unit_Test_Div).get_data
         for row in test_files:
-            self.assertAlmostEqual(self.calc.divide(row['Value 2'], row['Value 1']), float(row['Result']))
-            self.assertAlmostEqual(self.calc.result, float(row['Result']))
+            self.assertAlmostEqual(self.calc.divide(row[StaticVariable.value_2], row[StaticVariable.value_1]), float(row[StaticVariable.result]))
+            self.assertAlmostEqual(self.calc.result, float(row[StaticVariable.result]))
 
     def test_multiplication_method_calc(self):
-        test_files = CsvReader("/src/files/Unit Test Mult.csv").get_data
+        test_files = CsvReader(StaticVariable.unit_Test_Mult).get_data
         for row in test_files:
-            self.assertEqual(self.calc.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calc.result, int(row['Result']))
+            self.assertEqual(self.calc.multiply(row[StaticVariable.value_1], row[StaticVariable.value_2]), int(row[StaticVariable.result]))
+            self.assertEqual(self.calc.result, int(row[StaticVariable.result]))
 
     def test_square_method_calc(self):
-        test_files = CsvReader("/src/files/Unit Test Square.csv").get_data
+        test_files = CsvReader(StaticVariable.unit_Test_Square).get_data
         for row in test_files:
-            self.assertAlmostEqual(self.calc.square(row['Value 1']), float(row['Result']))
-            self.assertAlmostEqual(self.calc.result, float(row['Result']))
+            self.assertAlmostEqual(self.calc.square(row[StaticVariable.value_1]), float(row[StaticVariable.result]))
+            self.assertAlmostEqual(self.calc.result, float(row[StaticVariable.result]))
 
     def test_squareroot_method_calc(self):
-        test_files = CsvReader("/src/files/Unit Test Square root.csv").get_data
+        test_files = CsvReader(StaticVariable.unit_Test_SquareRoot).get_data
         for row in test_files:
-            self.assertAlmostEqual(self.calc.squareroot(row['Value 1']), float(row['Result']))
-            self.assertAlmostEqual(self.calc.result, float(row['Result']))
+            self.assertAlmostEqual(self.calc.squareroot(row[StaticVariable.value_1]), float(row[StaticVariable.result]))
+            self.assertAlmostEqual(self.calc.result, float(row[StaticVariable.result]))
 
     def tearDown(self) -> None:
         pass
